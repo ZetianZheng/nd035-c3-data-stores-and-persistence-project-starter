eab17ebde22050f9e14a88d7ad42b532487c2bb0
ZetianZheng <ztn.zheng@gmail.com>
2022-04-07 12:12:59 -0400

    fixed s4ljLogger

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
@@ -1,5 +1,7 @@
 package com.udacity.jdnd.course3.critter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class CritterApplication {
-
+//	private static final Logger logger = LoggerFactory.getLogger(CritterApplication.class);
 	public static void main(String[] args) {
+//		logger.debug("APPLICATION STARTED");
 		SpringApplication.run(CritterApplication.class, args);
 	}
 
